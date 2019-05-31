fa44220210fe789587bd3c987e1ca441d8538ba6
Gabriel <gabriel.zanatto2@gmail.com>
2019-05-31 08:29:00 -0300

    Initial Commit

---
+++ Conecta/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conecta
+//
+//  Created by Gabriel Zanatto Salami on 31/05/19.
+//  Copyright © 2019 Gabriel Zanatto Salami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
