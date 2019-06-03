c0c97d07221f78536d9b9faf0a2635a36e3f52a2
Gabriel <gabriel.zanatto2@gmail.com>
2019-06-03 09:28:35 -0300

    iniciando config na loginViewController

--- Conecta/Controller/LoginViewController.swift
+++ Conecta/Controller/LoginViewController.swift
@@ -10,8 +10,9 @@ import UIKit
 import Firebase
 import FirebaseDatabase
 import FirebaseAuth
+import FirebaseUI
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, FUIAuthDelegate {
 
 
     // Criando referencia a autenticacao
@@ -19,6 +20,14 @@ class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        FirebaseApp.configure()
+        let authUI = FUIAuth.defaultAuthUI()
+        authUI?.delegate = self
+        let providers: [FUIAuthProvider] = [
+            FUIGoogleAuth(),
+            FUIEmailAuth(),
+        ]
+        authUI?.providers = providers
         // Do any additional setup after loading the view.
         usuario.createUser(withEmail: "joao@gmail.com", password: "joao1234") { (usuario, erro) in
             guard let user = usuario?.user else { return }
