c19805a046c7f63d599e04caeca37bafa69c1eb5
Gabriel <gabriel.zanatto2@gmail.com>
2019-06-05 16:18:37 -0300

    add perfil controller, remover adicionar.storyboard...

---
+++ Conecta/Controller/PerfilViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PerfilViewController.swift
+//  Conecta
+//
+//  Created by Gabriel Zanatto Salami on 05/06/19.
+//  Copyright © 2019 Gabriel Zanatto Salami. All rights reserved.
+//
+
+import UIKit
+
+class PerfilViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //performSegue(withIdentifier: "entrar", sender: nil)
+        
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
