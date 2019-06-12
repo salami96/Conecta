883f9ff86101c63419da279561bf15b05ac8f9d1
Bruno <bruno.adamm@gmail.com>
2019-06-12 19:10:01 -0300

    wip
    wip

--- Conecta/Controller/FeedDetailViewController.swift
+++ Conecta/Controller/FeedDetailViewController.swift
@@ -10,19 +10,55 @@ import UIKit
 
 class FeedDetailViewController: UIViewController {
 
-    @IBOutlet weak var bannerImage: UIImageView!
-    @IBOutlet weak var perfilImage: UIImageView!
-    @IBOutlet weak var perfilView: UIView!
-    @IBOutlet weak var interessesView: UIView!
+
+    @IBOutlet weak var topBannerView: UIView!
+    @IBOutlet weak var contatoView: UIView!
+    @IBOutlet weak var ratingView: UIView!
+    
+    @IBOutlet weak var bioView: UIView!
+    @IBOutlet weak var imagemPerfil: UIImageView!
+//    @IBOutlet weak var navigation: UINavigationItem!
+    @IBOutlet weak var email: UILabel!
+    @IBOutlet weak var nome: UILabel!
+    @IBOutlet weak var telefone: UILabel!
+    @IBOutlet weak var capa: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        bannerImage.layer.cornerRadius = 5
-        perfilImage.layer.cornerRadius = 50;
-        perfilImage.layer.borderColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0);
-        perfilImage.layer.borderWidth = 1;
-        interessesView.layer.cornerRadius = 5
-        perfilView.layer.cornerRadius = 5
-        // Do any additional setup after loading the view.
+        
+        topBannerView.layer.cornerRadius = 5
+        topBannerView.layer.shadowColor = UIColor.gray.cgColor
+        topBannerView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        topBannerView.layer.shadowRadius = 12.0
+        topBannerView.layer.shadowOpacity = 0.1
+        
+        imagemPerfil.layer.cornerRadius = 50
+        imagemPerfil.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        imagemPerfil.layer.borderWidth = 1
+        
+        bioView.layer.cornerRadius = 5
+        bioView.layer.shadowColor = UIColor.gray.cgColor
+        bioView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        bioView.layer.shadowRadius = 12.0
+        bioView.layer.shadowOpacity = 0.1
+        
+        contatoView.layer.cornerRadius = 5
+        contatoView.layer.shadowColor = UIColor.gray.cgColor
+        contatoView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        contatoView.layer.shadowRadius = 12.0
+        contatoView.layer.shadowOpacity = 0.1
+        
+        ratingView.layer.cornerRadius = 5
+        ratingView.layer.shadowColor = UIColor.gray.cgColor
+        ratingView.layer.shadowOffset = CGSize(width: 0.0, height: 0.0)
+        ratingView.layer.shadowRadius = 12.0
+        ratingView.layer.shadowOpacity = 0.1
+        
+//        editBioButton.layer.cornerRadius = 5
+//        editContatoButton.layer.cornerRadius = 5
+        
+//        blurReference = Blur(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: view.bounds.size.height))
+//        blurReference.onClick = presentLogin
     }
     
 
